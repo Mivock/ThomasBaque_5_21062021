40445a3102fb4df80230875e5a96e4f0301e6825
Mivock <76434164+Mivock@users.noreply.github.com>
2021-06-21 17:59:50 +0200

    index.js avec get et mise en page html des cameras

---
+++ js/index.js
@@ -0,0 +1,20 @@
+//Fonction GET pour récuperer les data
+async function cameraPreview() {
+  fetch(url)
+    .then((response) => response.json())
+    .then((response) => {
+      for (let camera of response) {
+        //pour chaque objet de la reponse alors...
+        document.querySelector("#produits").innerHTML += `<div class="card"> 
+                        <img src="${camera.imageUrl}" class="card-img-top" alt="teste alternatif"/>
+                        <div class="card-body">
+                            <h5 class="card-title">${camera.name}</h5>
+                            <p class="card-text">${camera.description}</p> 
+                            <a href="item.html?id=${camera._id}" class="btn btn-warning">En savoir plus</a>
+                        </div>
+                    </div>`;
+      }
+    });
+}
+
+cameraPreview(); //démarrage de la fonction
